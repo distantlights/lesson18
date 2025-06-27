6f7f3aa32c0daa6b6509ad4d2a77a987fdeff916
distantlights <mar.babunashvili@gmail.com>
2025-06-27 07:45:29 -0700

    made half work

---
+++ script.js
@@ -0,0 +1,46 @@
+//-------------ფუნქცია სტრინგით-----------
+
+function checkBooleanString(value){
+    if (value === "true"){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(checkBooleanString("true"))
+console.log(checkBooleanString("false"))
+
+//მოკლე ვერსია
+
+function checkBooleanSttring(value){
+    return value === "true"
+}
+console.log(checkBooleanSttring("true"));
+
+//------------დოლარიდან ლარში--------------
+
+function convertUsdToGel(usdAmount, gelRate){
+    return usdAmount * gelRate
+}
+
+console.log(convertUsdToGel(100, 2.70));
+
+//------------ვალუტა---------------------
+
+function getCountryByCurrencyCode(code){
+    const currencyMap={
+        USD: "United States",
+        EUR: "Europian Union",
+        GEL: "Georgia"
+    };
+
+    if (currencyMap[code]){
+        return currencyMap[code];
+    } else {
+        return "unknow currency";
+    }
+}
+
+console.log(getCountryByCurrencyCode("USD"));
+
